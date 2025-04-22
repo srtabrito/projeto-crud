68ec3113b35f97286cb402c7764914491cc19f65
Ivna <paulinneivna32@gmail.com>
2025-04-22 00:23:52 -0300

    Criação da classe RespostaModelo

---
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/modelo/RespostaModelo.java
@@ -0,0 +1,14 @@
+package br.com.anm.projeto_crud.produtos.modelo;
+ 
+ import org.springframework.stereotype.Component;
+ 
+ import lombok.Getter;
+ import lombok.Setter;
+ 
+ @Component
+ @Getter
+ @Setter
+ 
+ public class RespostaModelo {
+     private String resposta;
+ }
