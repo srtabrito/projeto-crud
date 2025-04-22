7c4feb66b69e9dc0a25a7207392cbe74c25ee274
Ivna <paulinneivna32@gmail.com>
2025-04-22 00:24:50 -0300

    Criação da rota /listar

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
@@ -1,13 +1,25 @@
 package br.com.anm.projeto_crud.produtos.controle;
- 
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.RestController;
- 
- @RestController
- 
- public class ProdutoControle {
-     @GetMapping("/")
-     public String rota(){
-         return "A API está funcionando!";
-     }
- }
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
+import br.com.anm.projeto_crud.produtos.servico.ProdutoServico;
+
+@RestController
+public class ProdutoControle {
+
+    @Autowired
+    private ProdutoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return ps.listar();
+    }
+
+    @GetMapping("/")
+    public String rota() {
+        return "A API está funcionando!";
+    }
+}
