889c4ecc36f088f95c39997e32455a7c317cc213
Ivna <paulinneivna32@gmail.com>
2025-04-22 00:22:45 -0300

    Criação método listar() do serviço

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/servico/ProdutoServico.java
@@ -1,5 +1,18 @@
 package br.com.anm.projeto_crud.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
+import br.com.anm.projeto_crud.produtos.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
-    
+
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    public Iterable<ProdutoModelo> listar() {
+        return pr.findAll();
+    }
 }
