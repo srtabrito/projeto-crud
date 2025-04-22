1d66ffbc5b5b2a8a67943740d62eaa0629ce8526
Ivna <paulinneivna32@gmail.com>
2025-04-22 00:41:34 -0300

    Bootstrap Instalado; Componentes Tabela e Formulário Criado

---
+++ frontend/front/src/Tabela.js
@@ -0,0 +1,24 @@
+function Tabela(){
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Marca</th>
+                    <th>Selecionar</th>
+                </tr>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </thead>
+        </table>
+    )
+}
+
+export default Tabela;
