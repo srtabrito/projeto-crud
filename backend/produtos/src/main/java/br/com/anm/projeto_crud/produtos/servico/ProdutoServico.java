69324527007c9bf3ed5e59c0ed412ff874d5a0c0
Ivna <paulinneivna32@gmail.com>
2025-04-22 00:37:10 -0300

    Criação do front e alteração do ProdutoControle e ProdutoServico

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/servico/ProdutoServico.java
@@ -1,38 +1,48 @@
 package br.com.anm.projeto_crud.produtos.servico;
- 
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.http.HttpStatus;
- import org.springframework.http.ResponseEntity;
- import org.springframework.stereotype.Service;
- 
- import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
- import br.com.anm.projeto_crud.produtos.modelo.RespostaModelo;
- import br.com.anm.projeto_crud.produtos.repositorio.ProdutoRepositorio;
- 
- @Service
- public class ProdutoServico {
- 
-     @Autowired
-     private ProdutoRepositorio pr;
- 
-     @Autowired
-     private RespostaModelo rm;
- 
-     public Iterable<ProdutoModelo> listar(){
-         return pr.findAll();
-     }
- 
-     //Cadastrar produtos
-     public ResponseEntity<?> cadastrar(ProdutoModelo pm){
-         if(pm.getNome().equals("")){
-             rm.setResposta("O nome do produto é obrigatório!");
-             return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
-         } else if(pm.getMarca().equals("")){
-             rm.setResposta("O nome da marca do produto é obrigatório!");
-             return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
-         } else {
-             return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.CREATED);
-         }
-     }
- 
- }
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
+import br.com.anm.projeto_crud.produtos.modelo.RespostaModelo;
+import br.com.anm.projeto_crud.produtos.repositorio.ProdutoRepositorio;
+
+@Service
+public class ProdutoServico {
+    
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    @Autowired
+    private RespostaModelo rm;
+
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
+
+    public ResponseEntity <?> cadastrarAlterar(ProdutoModelo pm, String acao){
+        if(pm.getNome().equals("")){
+            rm.setResposta("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setResposta("A marca do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            if(acao.equals("cadastrar")){
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            }else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
+                
+            }
+        }
+    }
+
+    public ResponseEntity<RespostaModelo> remover(long codigo){
+        pr.deleteById(codigo);
+        rm.setResposta("O produto foi removido com sucesso!");
+        return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+    }
+
+}
