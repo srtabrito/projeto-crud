b0182ba448211611ab9e041dd737199c8f7b5a82
Ivna <paulinneivna32@gmail.com>
2025-04-22 00:27:08 -0300

    Rota para cadastrar e deletar criado

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
@@ -1,25 +1,33 @@
 package br.com.anm.projeto_crud.produtos.controle;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
-import br.com.anm.projeto_crud.produtos.servico.ProdutoServico;
-
-@RestController
-public class ProdutoControle {
-
-    @Autowired
-    private ProdutoServico ps;
-
-    @GetMapping("/listar")
-    public Iterable<ProdutoModelo> listar() {
-        return ps.listar();
-    }
-
-    @GetMapping("/")
-    public String rota() {
-        return "A API está funcionando!";
-    }
-}
+ 
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.RequestBody;
+ import org.springframework.web.bind.annotation.RestController;
+ 
+ import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
+ import br.com.anm.projeto_crud.produtos.servico.ProdutoServico;
+ 
+ @RestController
+ public class ProdutoControle {
+ 
+     @Autowired
+     private ProdutoServico ps;
+ 
+     @PostMapping("/cadastrar")
+     public ResponseEntity<?> cadastar(@RequestBody ProdutoModelo pm){
+         return ps.cadastrar(pm);
+     }
+ 
+     @GetMapping("/listar")
+     public Iterable<ProdutoModelo> listar(){
+         return ps.listar();
+     }
+ 
+     @GetMapping("/")
+     public String rota(){
+         return "A API está funcionando!";
+     }
+ }
