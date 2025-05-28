92666567e62b8ffb189830b8ab1e02defc246537
Ivna <paulinneivna32@gmail.com>
2025-05-27 22:20:10 -0300

    Inserção dos ícones e funcionamento dos botões

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/ProdutosApplication.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/ProdutosApplication.java
@@ -3,7 +3,6 @@ package br.com.anm.projeto_crud.produtos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ProdutosApplication {
 
--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/repositorio/ProdutoRepositorio.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/repositorio/ProdutoRepositorio.java
@@ -1,11 +1,9 @@
 package br.com.anm.projeto_crud.produtos.repositorio;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
 
-@Repository
-public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long>{
-    
+public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long> {
+
 }
